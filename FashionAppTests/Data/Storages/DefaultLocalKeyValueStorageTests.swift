dfa499dc3e09a87e2f160caf013f1fa6ddb5af27
Azat <kaumov.a.r@gmail.com>
2022-05-28 16:37:31 +0200

    Unit Tests: Add tests for DefaultLocalKeyValueStorage

---
+++ FashionAppTests/Data/Storages/DefaultLocalKeyValueStorageTests.swift
@@ -0,0 +1,49 @@
+//
+//  DefaultLocalKeyValueStorageTests.swift
+//  FashionAppTests
+//
+//  Created by Azat Kaiumov on 28.05.22.
+//
+
+import XCTest
+@testable import FashionApp
+
+class DefaultLocalKeyValueStorageTests: XCTestCase {
+    
+    var storage: LocalKeyValueStorage!
+    
+    override func setUp() async throws {
+
+        storage = DefaultLocalKeyValueStorage()
+    }
+    
+    func test_get_not_existing_key() {
+        
+        XCTAssertNil(storage.getItem(key: "test"))
+    }
+    
+    func test_remove_not_existing_key() {
+        
+        XCTAssertNil(storage.removeItem(key: "test"))
+    }
+    
+    func test_remove_existing_key() {
+        
+        storage.setItem(key: "test1", value: "value1")
+        storage.setItem(key: "test2", value: "value2")
+        
+        storage.removeItem(key: "test1")
+        
+        XCTAssertNil(storage.getItem(key: "test1"))
+        XCTAssertEqual(storage.getItem(key: "test2"), "value2")
+    }
+
+    func test_set_get_item() {
+        
+        storage.setItem(key: "test1", value: "value1")
+        storage.setItem(key: "test2", value: "value2")
+        
+        XCTAssertEqual(storage.getItem(key: "test1"), "value1")
+        XCTAssertEqual(storage.getItem(key: "test2"), "value2")
+    }    
+}
